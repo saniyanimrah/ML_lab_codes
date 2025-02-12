7e8a18b4a03c4c4689119b10502f4f2e0f3884d2
saniyanimrah <saniyanimrah75@gmail.com>
2025-02-12 15:00:19 +0530

    Add files via upload

---
+++ 1ml.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 12 13:42:16 2025
+
+@author: pc
+"""
+import statistics
+def calculate_mean(data):
+    return sum(data)/len(data)
+def calculate_median(data):
+    sorted_data =sorted(data)
+    n=len(sorted_data)
+    if n % 2 == 0:
+        middle1 = sorted_data[n // 2-1]
+        middle2 = sorted_data[n // 2]
+        return (middle1 + middle2)/2 
+    else:
+        return sorted_data[n //2 ]
+def calculate_mode(data): 
+    return statistics.mode(data) 
+def calculate_variance(data):
+    mean_value = calculate_mean(data)
+    squared_diff_sum = sum((x-mean_value)** 2 for x in data)
+    return squared_diff_sum /(len(data) - 1)
+def calculate_standard_deviation(data): 
+    variance_value = calculate_variance(data)
+    return variance_value ** 0.5
+dataset = [10,20,30,40,50] 
+mean_value=calculate_mean(dataset)
+median_value=calculate_median(dataset)
+mode_value=calculate_mode(dataset)
+variance_value=calculate_variance(dataset)
+std_deviation_value=calculate_standard_deviation(dataset)
+print(f"Dataset:{dataset}")
+print(f"Mean:{mean_value:.2f}")
+print(f"Mode:{mode_value:.2f}")
+print(f"Median:{median_value:.2f}")
+print(f"Variance:{variance_value:.2f}")
+print(f"Standard deviation:{std_deviation_value:.2f}")
+
+        
