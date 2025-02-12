6b6f0b32c0f5c552eeba2e3826376877e000cb5c
saniyanimrah <saniyanimrah75@gmail.com>
2025-02-12 15:36:18 +0530

    Add files via upload

---
+++ 2.2 ml.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 12 15:10:05 2025
+
+@author: pc
+"""
+
+import numpy as np
+arr1d = np.array([1,2,3])
+print(f"1d array:{arr1d}")
+arr2d = np.array([[1,2,3],[4,5,6]])
+print(f"2d array:{arr2d}")
