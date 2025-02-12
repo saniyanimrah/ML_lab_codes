6a610446f97479f676153416475828e72236349b
saniyanimrah <saniyanimrah75@gmail.com>
2025-02-12 15:35:54 +0530

    Add files via upload

---
+++ 2.1 ml.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 12 14:58:08 2025
+
+@author: pc
+"""
+
+import math
+sqrt_value = math.sqrt(25)
+print(f"sqrt 25:{sqrt_value:.2f}")
+factorial_value=math.factorial(5)
+print(f"fact:{factorial_value:.2f}")
+angle_radians=math.radians(30)
+sine_value=math.sin(angle_radians)
+print(f"sine:{sine_value:.2f}")
